71b51eb859e19d7469bc120e6ab2e544c7945419
Yenimireddy Vignesh <vignesh22032006@gmail.com>
2026-09-15 09:56:02 +0530

    first commit

---
+++ HashSetDemo.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import java.util.*;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet<Integer> set =new HashSet<>();
+		set.add(10);
+		set.add(20);
+		set.add(30);
+		set.add(40);
+		System.out.println(set);
+	}
+
+}
